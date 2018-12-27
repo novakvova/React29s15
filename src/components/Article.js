347ac4f43f069295798a3bceb36acd9b4a4dcca5
novakvova <novakvova@gmail.com>
2018-12-27 17:00:05 +0200

    Bind on click handler

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,17 +1,52 @@
 import React from 'react'
 
 
-function Article(props) {
-    console.log("---Article props---", props);
-    const {article} = props;
-    const body=<section>{article.text}</section>
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>create date: {(new Date(article.date)).toDateString()}</h3>
+class Article extends React.Component {
 
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        // const {semen} = this.props;
+        // let t=this.props.semen;
+        // console.log('------props ctor-------', props);
+        // console.log('------props ctor SEMEN-------', semen);
+        //this.btnHandleClose=this.btnHandleClose.bind(this);
+    }
+
+    state = {
+        isOpen: true
+    };
+    render() {
+        console.log("---Article props---", this.props);
+        const { article } = this.props;
+        const body = <section>{article.text}</section>
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={this.btnHandleClose}>
+                        {this.state.isOpen ? 'close' : 'open'} 
+                    </button>
+                </h2>
+                {body}
+                <h3>create date: {(new Date(article.date)).toDateString()}</h3>
+
+            </div>
+        );
+    }
+
+    btnHandleClose =() => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+        //console.log("------", 'clicked');
+    }
+
+    // btnHandleClose = function() {
+    //     console.log("---this btn----",this);
+    //     this.setState({
+    //         isOpen: !this.state.isOpen
+    //     });
+    //     //console.log("------", 'clicked');
+    // }
 }
 export default Article;
